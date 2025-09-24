cf2c521df2fbf65b21814813b1baa8e1071f85b4
siva <tandrusaichowdary@gmail.com>
2025-09-24 19:27:26 +0530

    variables

--- 08-special-variables.sh
+++ 08-special-variables.sh
@@ -7,6 +7,9 @@ echo "script name: $0" #dynamic
 
 #self explainatory 
 
-echo "current directory: $PWD
-echo "who is running this: $USER
-echo home diectory: $HOME
+echo "current directory: $PWD"
+echo "who is running this: $USER"
+echo "home diectory: $HOME"
+echo "pid of the script: $$" #$$ script pid
+sleep 50 & #it will go to background 
+echo "pid of the last command in background is: $!" #$! pid value
