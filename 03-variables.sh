ed32a5b190359fae26320fdac3507b49a1448b66
siva <tandrusaichowdary@gmail.com>
2025-09-24 12:53:02 +0530

    some message

---
+++ 03-variables.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+PERSON1=Sai
+PERSON2=Sri
+
+echo "$PERSON1:: Hi $PERSON2, How are you?"
+echo "$PERSON2:: Hello $PERSON1, I'm fine. How are you?"
+echo "$PERSON1:: What are you cooking recently?"
+echo "$PERSON2:: I'm cooking biryani $PERSON1"
