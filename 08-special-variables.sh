c36597d820232018f5460e803e278668f5c37ee3
siva <tandrusaichowdary@gmail.com>
2025-09-24 19:22:58 +0530

    variables

--- 08-special-variables.sh
+++ 08-special-variables.sh
@@ -3,4 +3,10 @@
 echo "all variables passed to the script: $@"
 echo "all variables passed to the script: $*"
 
-echo "script name: $0"
+echo "script name: $0" #dynamic 
+
+#self explainatory 
+
+echo "current directory: $PWD
+echo "who is running this: $USER
+echo home diectory: $HOME
