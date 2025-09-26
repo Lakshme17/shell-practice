248b6d57c13502aee0e9596ff3953988e68c303a
siva <tandrusaichowdary@gmail.com>
2025-09-26 11:08:58 +0530

    conditions

--- 12-installation.sh
+++ 12-installation.sh
@@ -7,7 +7,7 @@ if [ $USERID -ne 0 ]; then
    exit 1 # exit means - we can exit from excution of continution 
 fi 
 
-dnf install mysq1 -y 
+dnfggggg install mysql -y 
 
 if [ $? -ne 0 ]; then
    echo "ERROR:: Installing MySQL is failure"
