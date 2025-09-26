156f5c185c7918b7229dff53f59f432a180124a4
siva <tandrusaichowdary@gmail.com>
2025-09-26 11:06:32 +0530

    conditions

---
+++ 12-installation.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+USERID=$(id -u)
+
+if [ $USERID -ne 0 ]; then
+   echo "ERROR:: Please run this script with root privelege"
+   exit 1 # exit means - we can exit from excution of continution 
+fi 
+
+dnf install mysql -y 
+
+if [ $? -ne 0 ]; then
+   echo "ERROR:: Installing MySQL is failure"
+   exit 1
+else 
+   echo "Installing MySQL is success"
+fi 
+
