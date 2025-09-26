9cb4c444b3924f49e39caf361a309587b393caf7
siva <tandrusaichowdary@gmail.com>
2025-09-26 10:08:44 +0530

    conditions

--- 10-conditions.sh
+++ 10-conditions.sh
@@ -2,8 +2,23 @@
 
 NUMBER=$1
 
+#if [ $NUMBER -lt 17 ]; then
+#    echo "Given number $NUMBER is less than 17"
+#else
+ #   echo "Given number $NUMBER is greater than or equal to 17"
+#fi        
+
 if [ $NUMBER -lt 17 ]; then
     echo "Given number $NUMBER is less than 17"
+elif [ $NUMBER -eq 17 ]; then
+    echo "Given number $NUMBER is equal to 17"   
 else
     echo "Given number $NUMBER is greater than or equal to 17"
-fi        
+fi     
+
+
+
+# -gt -> greater than
+# -lt -> less than
+# -eq -> equal to
+# -ne ->not equal to
