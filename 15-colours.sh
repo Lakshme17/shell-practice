c701e95c89711e74c26cedd3558945055c98973f
siva <tandrusaichowdary@gmail.com>
2025-09-26 12:20:53 +0530

    conditions

---
+++ 15-colours.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+
+
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[om"
+
+USERID=$(id -u)
+
+if [ $USERID -ne 0 ]; then
+    echo "error:: please run this script with root privilege"
+    exit 1 
+fi
+
+#$1 represents the first argument passed to the script when it is executed.
+VALIDATE(){
+     if [ $1 -ne 0 ]; then
+         echo -e "installing $2 .....$R failure $N"
+         exit 1
+     else
+         echo -e installing $2 ......$G success $N"
+     fi        
+ }
+ 
+dnf list installed mysql
+if [ $? -ne 0 ]; then
+    dnf install mysql -y 
+    VALIDATE $? "MySQL"
+else
+    echo -e "MySQL already exit ...$Y skipping $N"
+fi 
+
+dnf list installed nginx
+if [ $? -ne 0 ]; then
+    dnf install nginx -y 
+    VALIDATE $? "Nginx"
+else
+    echo -e "Nginx already exit ...$Y skipping $N"
+fi 
+
+dnf list installed python3
+if [ $? -ne 0 ]; then
+    dnf install python3 -y
+    VALIDATE $? "python3"
+else
+    echo -e "Python3 already exist ... $Y SKIPPING $N"
+fi
