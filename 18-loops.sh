02ac6e6392eaee83c44453dbdea524b78336f867
siva <tandrusaichowdary@gmail.com>
2025-09-27 22:47:44 +0530

    loops

--- 18-loops.sh
+++ 18-loops.sh
@@ -1,50 +1,45 @@
 #!/bin/bash
 
+# Define color codes
+R="\033[31m"
+G="\033[32m"
+Y="\033[33m"
+N="\033[0m"
 
-
-R="\e[31m"
-G="\e[32m"
-Y="\e[33m"
-N="\e[0m"
-
+# Check if script is run as root
 USERID=$(id -u)
-
-LOGS_FOLDER="/var/log/shell-script"
-SCRIPT_NAME=$( echo $0 | cut -d "." -f1 )
-LOG_FILE="$LOGS_FOLDER/$SCRIPT_NAME.log"
-
-
-mkdir -p $LOGS_FOLDER
-echo "Script started executed at: $(date)" | tee -a $LOG_FILE
-
-if [ $USERID -ne 0 ]; then
-    echo "error:: please run this script with root privilege"
-    exit 1 
+if [ "$USERID" -ne 0 ]; then
+    echo -e "${R}Error:${N} Please run this script with root privileges"
+    exit 1
 fi
 
-#$1 represents the first argument passed to the script when it is executed
-VALIDATE(){
-     if [ $1 -ne 0 ]; then
-         echo -e "installing $2 .....$R failure $N" | tee -a $LOG_FILE
-         exit 1
-     else
-         echo -e "installing $2 ......$G success $N" | tee -a $LOG_FILE
-     fi        
- }
+# Setup logging
+LOGS_FOLDER="/var/log/shell-script"
+SCRIPT_NAME="$(basename "$0")"
+SCRIPT_NAME="${SCRIPT_NAME%%.*}"
+LOG_FILE="$LOGS_FOLDER/$SCRIPT_NAME.log"
 
-#$@ special variables 
+mkdir -p "$LOGS_FOLDER"
+echo "Script started at: $(date)" | tee -a "$LOG_FILE"
 
-for package in $@
-do  
-    #CHECK PACKAGE IS ALREADY INSTALLED OR NOT
-   dnf list installed $package &>>$LOG_FILE
+# Validation function
+VALIDATE() {
+    if [ "$1" -ne 0 ]; then
+        echo -e "Installing $2 ....... ${R}Failure${N}" | tee -a "$LOG_FILE"
+        exit 1
+    else
+        echo -e "Installing $2 ....... ${G}Success${N}" | tee -a "$LOG_FILE"
+    fi        
+}
 
-   #IF EXIT Status is 0, already istalled, -ne 0 needs to install it
-   if [ $? -ne 0 ]; then
-       dnf install $package -y &>>$LOG_FILE
-       VALIDATE $? "$package"
-   else
-
-      echo -e "$package already installed .....$Y Skipping $N"
-   fi
+# Install packages passed as arguments
+for package in "$@"; do
+    # Check if the package is already installed
+    dnf list installed "$package" &>>"$LOG_FILE"
+    if [ "$?" -ne 0 ]; then
+        dnf install "$package" -y &>>"$LOG_FILE"
+        VALIDATE "$?" "$package"
+    else
+        echo -e "$package is already installed ....... ${Y}Skipping${N}" | tee -a "$LOG_FILE"
+    fi
 done
